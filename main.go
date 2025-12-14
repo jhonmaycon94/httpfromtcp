f1b0e701248c0d5e14405e413f47f7d373a346f5
Jhon Santos <jhonmaycon94@gmail.com>
2025-12-14 18:23:15 -0300

    refatora codigo

--- main.go
+++ main.go
@@ -3,9 +3,41 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer file.Close()
+		defer close(ch)
+
+		line := ""
+		for {
+			chunk := make([]byte, 8)
+			i, err := file.Read(chunk)
+			if err != nil {
+				break
+			}
+			chunk = chunk[:i]
+			if n := bytes.IndexByte(chunk, '\n'); n != -1 {
+				line += string(chunk[:n])
+				chunk = chunk[n+1:]
+				ch <- line
+				line = ""
+			}
+
+			line += string(chunk)
+		}
+
+		if len(line) != 0 {
+			ch <- line
+		}
+	}()
+	return ch
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -13,26 +45,8 @@ func main() {
 		log.Fatal("Error", "Error", err)
 	}
 
-	line := ""
-	for {
-		chunk := make([]byte, 8)
-		i, err := file.Read(chunk)
-		if err != nil {
-			break
-		}
-		chunk = chunk[:i]
-		if n := bytes.IndexByte(chunk, '\n'); n != -1 {
-			line += string(chunk[:n])
-			chunk = chunk[n+1:]
-
-			fmt.Printf("read: %s\n", line)
-			line = ""
-		}
-
-		line += string(chunk)
-	}
-
-	if len(line) != 0 {
+	lines := getLinesChannel(file)
+	for line := range lines {
 		fmt.Printf("read: %s\n", line)
 	}
 }
